197b45bcaac69d7aff112e5cb44844a9a34bcbc1
abayacoder <73934658+abayacoder@users.noreply.github.com>
2020-11-22 18:29:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,9 +37,14 @@ function setup() {
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
-
-	Engine.run(engine);
+	
+	box1=bodies.rectangle(300,630,20,60{isStatic:true,restitution:1}
+        box2=bodies.rectangle(500,630,20,60{isStatic:true,restitution:1}
+	box3=bodies.rectangle(400,650,180,20{isStatic:true,restitution:1}
+			      
+	console.log(box1)		      
+			      
+       Engine.run(engine);
   
 }
 
@@ -50,6 +55,11 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
+
+rect(box1.position.x,box1.posittion.y,20,60)
+rect(box2.position.x,box2.posittion.y,20,60)
+rect(box3.position.x,box3.posittion.y,180,20)
+ 
  
 }
 
@@ -58,14 +68,21 @@ function keyPressed() {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
     Matter.Body.setStatic(packageBody,false)
 	redbox.isStatic(20,200)
-	sides=(100,20)
-	//when we press the down arrow  and the package stay inside the redbox
+	sides=(100,20)package stay inside the redbox
 	if (DOWN_ARROW(package)){
 		package.restitution=0
 	}
-
-  }
-}
-
-
-
+	 if(keyCode===LEFT_ARROW){
+	helicopterSprite.x=helicopterSprite.x-20;
+	translante={x:-20,y:0}
+	Matter.Body.translate(packageBody,translation)
+	
+	 } esle if(keyCode===RIGHT_ARROW){
+	helicopterSprite.x=helicopterSprite.x+20;
+	translante={x:,20,y:0}
+	Matter.Body.translate(packageBody,translation)
+	 
+	 }esle if(keyCode===DOWN_ARROW){
+           Matter.Body.setStatic(packageBody,false);
+ }
+ }
